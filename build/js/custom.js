3c5d13ed82dbf522babe9c80cc3a31ee8067320e
Nikita Ostapenko <nikitaostapenko12@gmail.com>
2017-10-05 20:30:44 +0300

    Add js for sliders and fixes some bugs

--- build/js/custom.js
+++ build/js/custom.js
@@ -2,7 +2,37 @@
     "use strict";
     $(document).ready(function () {
 
-        console.log($('body'));
+        var betElements = $('.digits li').not( '.li-bg' );
+
+        function newPosition(elem){
+
+            var leftPosition = +elem.width()/2 - elem.closest('.digits').find('.li-bg').width()/2;
+
+            elem.closest('.digits').find('.li-bg').stop().animate({
+                'left' : elem.position().left + leftPosition
+            }, 300);
+        }
+
+        betElements.on('click', function(){
+            if(!$(this).hasClass('checked')){
+
+                $(this).closest('.digits').find('.checked').removeClass('checked');
+                $(this).addClass('checked');
+
+                newPosition($(this));
+
+            }
+        });
+
+        $(window).on('resize orientationchage load', function(){
+            newPosition($('.settings-block-value-coin .checked'))
+        });
+        $(window).on('resize orientationchage load', function(){
+            newPosition($('.settings-block-value-bet .checked'))
+        });
+
+
+        
 
     });
 })(jQuery);
